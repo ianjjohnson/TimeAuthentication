86b912c1dba37312bfc9e13e42f4ecfdf58f330a
Ian Johnson <ianjjohnson@icloud.com>
2016-10-18 19:22:01 -0500

    Authenticating back and forth

--- testing.py
+++ testing.py
@@ -1,11 +1,19 @@
 from Crypto.Cipher import AES
+import time
 
 JITTER = 0
 
 N_0 = '1234567890123456'
 K   = '9876543210123456'
 
-E   = AES.new(K, AES.MODE_OFB, N_0)
+E   = AES.new(K, AES.MODE_CFB, N_0)
+delays = []
+
+
+ni = N_0
+for _ in range(1000):
+    ni = E.encrypt(ni)
+    delays.append(int.from_bytes(ni, byteorder="little") % 128)
 
 
 class Message:
@@ -19,46 +27,50 @@ class Message:
 
 class Client:
 
-    def __init__(self, name, key, n_not):
+    def __init__(self, name):
         self.inbox = []
         self.outbox = []
         self.name = name
-        self.E = AES.new(key, AES.MODE_CBC, n_not)
-        self.stream = self.E.encrypt(n_not)
+        self.i = 0
         self.expectedResponse = 0
         self.warningResponse  = 0
         self.time = 0
 
     def next_delay(self):
-        self.stream = E.encrypt(self.stream)
-        return int.from_bytes(self.stream, byteorder="little") % 128
+        self.i = self.i + 1
+        return delays[self.i]
 
-    def sendReply(self, m, t = -1, danger = False):
+    def sendReply(self, sender, t = -1, danger = False):
+
         if t == -1:
             t = self.time
+
         delay = self.next_delay()
         nextDelay = self.next_delay()
         self.expectedResponse = t + delay + nextDelay
         self.warningResponse = t + delay + nextDelay/2
-        response = Message(self, m.sender, t + (delay/2 if danger else delay), "Ping")
+        response = Message(self, sender, t + (delay/2 if danger else delay), "Ping")
         self.outbox.append(response)
 
     def addMessage(self, m):
         self.inbox.append(m)
 
     def handleMessage(self, m):
-        print(m.recipient.name, "got message: ")
-        print(m.content)
+        print(self.name, "got message: ", m.content)
 
-        if(self.time == self.expectedResponse):
+
+        t = (self.time-1 if (self.name == "Alice" and m.content != "Startup") else self.time)
+
+        if(t == self.expectedResponse):
             print("Authenticated")
-            self.sendReply(m)
-        elif(self.time == self.warningResponse):
+            self.sendReply(m.sender, t=self.expectedResponse)
+        elif(t == self.warningResponse):
             print("Danger Will Robinson")
-            self.sendReply(m, self.expectedResponse)
+            self.sendReply(m.sender, t=self.expectedResponse)
         else:
             print("Bad Message")
-            self.sendReply(m, self.expectedResponse, True)
+            print(t, self.expectedResponse)
+            self.sendReply(m.sender, t=self.expectedResponse)#, danger=True)
 
 
     def checkForMessage(self):
@@ -78,15 +90,20 @@ class Client:
         self.time = self.time + 1
 
 
+Alice = Client("Alice")
+Bob = Client("Bob")
+
+Alice.inbox.append(Message(Bob, Alice, 0, "Startup"))
+Bob.expectedResponse = Bob.next_delay()
+
+
+print(delays[1:5])
+
 def sim_loop():
 
     Alice.update()
     Bob.update()
 
-
-Alice = Client("Alice", N_0, K)
-Bob = Client("Bob", N_0, K)
-Alice.addMessage(Message(Bob, Alice, 0, "Ping"))
-
 while(True):
+    time.sleep(0.01)
     sim_loop()
